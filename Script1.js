a6c9c4c156c26bfa98055d5d930621efa4fce2eb
ArinaBalobanova <arinabalobanova29@gmail.com>
2025-05-02 21:16:41 +0300

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -1,1 +1,73 @@
-// JavaScript source code
+{
+  "instructors": [
+    {
+      "id": 12345678,
+      "name": "Анна Каренина",
+      "specialization": "Итальянская кухня",
+      "experience": 7,
+      "contact": {
+        "email": "anna.karenina@culinary.com",
+        "phone": "+7 (123) 456-7890",
+        "address": "ул. Кремлевская, д. 5, кв. 12"
+      },
+      "students": [
+        {
+          "id": 101,
+          "name": "Михаил Маркелов",
+          "skillLevel": "Продвинутый"
+        },
+        {
+          "id": 102,
+          "name": "Руслана Лисюткина",
+          "skillLevel": "Средний"
+        }
+      ]
+    },
+    {
+      "id": 23456789,
+      "name": "Валеев Сергей",
+      "specialization": "Французская кухня",
+      "experience": 10,
+      "contact": {
+        "email": "dmitry.sousov@culinary.com",
+        "phone": "+7 (987) 654-3210",
+        "address": "пр. Пушкина, д. 22, кв. 45"
+      },
+      "students": [
+        {
+          "id": 103,
+          "name": "Алексей Габов",
+          "skillLevel": "Профессиональный"
+        },
+        {
+          "id": 104,
+          "name": "Зарина Хубайбулина",
+          "skillLevel": "Начинающий"
+        },
+        {
+          "id": 105,
+          "name": "Сергей Волков",
+          "skillLevel": "Средний"
+        }
+      ]
+    },
+    {
+      "id": 34567892,
+      "name": "Анжела Пушкина",
+      "specialization": "Кондитерское искусство",
+      "experience": 15,
+      "contact": {
+        "email": "maria.tortikova@culinary.com",
+        "phone": "+7 (555) 123-4567",
+        "address": "ул. Парина, д. 3, кв. 7"
+      },
+      "students": [
+        {
+          "id": 106,
+          "name": "Дана Халикова",
+          "skillLevel": "Продвинутый"
+        }
+      ]
+    }
+  ]
+}
